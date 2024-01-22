040e87267668ce856aa9bc2682962194cb7a5b30
dimas <dimaspradipto@gmail.com>
2024-01-22 21:59:37 +0700

    create assets, config, model laundry, model promo, model shop, model user

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_typing_uninitialized_variables, prefer_const_constructors
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -9,8 +11,37 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
+    // ignore: non_constant_identifier_names
+    var GoogleFonts;
+    return MaterialApp(
+      theme: ThemeData(
+        primaryColor: Colors.green,
+        scaffoldBackgroundColor: Colors.white,
+        colorScheme: const ColorScheme.light(
+          primary: Colors.green,
+          secondary: Colors.greenAccent,
+        ),
+        textTheme: GoogleFonts.latoTextTheme(),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ButtonStyle(
+            backgroundColor: MaterialStateProperty.all(Colors.green),
+            shape: MaterialStatePropertyAll(
+              RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10),
+              ),
+            ),
+            padding: MaterialStateProperty.all(
+              EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+            ),
+            textStyle: MaterialStateProperty.all(
+              TextStyle(
+                fontSize: 15,
+              ),
+            ),
+          ),
+        ),
+      ),
+      home: const Scaffold(
         body: Center(
           child: Text('Hello World!'),
         ),
